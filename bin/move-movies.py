c05764032450fc53170e57feda61c9811420a211
Derek Bolt <derebolt@amazon.com>
2017-11-12 15:18:07 -0800

    Updated move-movies

--- bin/move-movies.py
+++ bin/move-movies.py
@@ -9,19 +9,20 @@ EXT=('mp4', 'mkv')
 
 files = []
 for (dirPath, dirNames, fileNames) in os.walk('.'):
-    for fileName in fileNames:
-        ext = re.search("\.([\w]+$)", fileName).group(1)
+    for f in fileNames:
+        ext = re.search("\.([\w]+$)", f).group(1)
         if ext is None or ext not in EXT:
             continue
 
-        name = re.search("(.*)\.[\d]{4}\.", fileName).group(1)
-        year = re.search("\.([\d]{4})\.", fileName).group(1)
+        name = re.search("(.*)\.[\d]{4}\.", f).group(1)
+        year = re.search("\.([\d]{4})\.", f).group(1)
+        quality = re.search("[\d]{4}\.([\d]{3,4}[\w]){0,1}\.", f).grou(1)
 
-        newName = "%s.%s.%s" % (name, year, ext)
-        oldPath = os.path.abspath(os.path.join(dirPath, fileName))
+        newName = "%s.%s.%s.%s" % (name, year, quality, ext)
+        oldPath = os.path.abspath(os.path.join(dirPath, f))
         newPath = os.path.join(PATH, newName)
 
-        print("Moving file from completed/%s to movies/%s" % (fileName, newName))
+        print("Moving file from completed/%s to movies/%s" % (f, newName))
         print("Approve? y or n")
         answer = raw_input()
         if answer != 'y':
